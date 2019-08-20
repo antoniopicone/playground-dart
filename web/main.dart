1295ef2ff3ca3f363fe5ef8dc97427014920614f
Antonio <info@antoniopicone.it>
2019-08-20 20:02:59 +0000

    Types and vars

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,26 @@
 // Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+int squareFunc(int n) {
+  return n*n;
+}
+
 main() {
-  print('Hello World!');  
+
+  var myObj = {
+    'arr': ['ciao','bell'],
+    'string': 'hello',
+    'number': 3
+  };
+
+  print('Hello World '+ myObj['number'].toString() );  
+
+  for (var object in myObj['arr']) {
+    print(object);
+  }
+
+  int n = 3;
+  print(n.toString()+'^2='+squareFunc(n).toString());
+
 }
