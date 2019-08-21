ae335b1f12ee2bcb74ca655a93add50ac9f3d3eb
Antonio <info@antoniopicone.it>
2019-08-21 09:14:29 +0000

    Async requests & gitignore updates

--- web/main.dart
+++ web/main.dart
@@ -2,8 +2,44 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert';
+import 'dart:async';
+import 'dart:html';
+
+String daJson;
+
+UListElement wordList = querySelector('#wordList');
+ButtonElement mybutton = querySelector('#getWords');
+
 int squareFunc(int n) {
   return n*n;
+}
+
+Future<void> makeRequest(Event _) async {
+  const path = 'https://www.dartlang.org/f/portmanteaux.json';
+  print('clicked');
+
+  try {
+    // Make the GET request
+    final jsonString = await HttpRequest.getString(path);
+    // The request succeeded. Process the JSON.
+    processResponse(jsonString);
+  } catch (e) {
+    // The GET request failed. Handle the error.
+    print('Couldn\'t open $path');
+    // wordList.children.add(LIElement()..text = 'Request failed.');
+  }
+  
+}
+
+// void processResponse(String jsonString) {
+//   daJson = json.decode(jsonString);
+//   print('ora '+daJson);
+// }
+void processResponse(String jsonString) {
+  for (final portmanteau in json.decode(jsonString)) {
+    wordList.children.add(LIElement()..text = portmanteau);
+  }
 }
 
 main() {
@@ -23,4 +59,11 @@ main() {
   int n = 3;
   print(n.toString()+'^2='+squareFunc(n).toString());
 
+  // var ajson = jsonEncode(myObj);
+  // print(ajson);
+
+  //querySelector('#test').onLoad.listen(makeRequest);
+  
+  mybutton.onClick.listen(makeRequest);
 }
+
